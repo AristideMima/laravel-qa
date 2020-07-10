357940f9b7c107ec565b4e760b950c07a4fb9646
aristidemima <aristidemima@gmail.com>
2020-07-10 02:36:57 -0500

    vote answers and view display plus laravel mix

--- app/Question.php
+++ app/Question.php
@@ -34,4 +34,20 @@ class Question extends Model
     {
         return $this->created_at->diffForHumans();
     }
+
+    //Display status
+    public function getStatusAttribute()
+    {
+        if($this->answers > 0)
+        {
+            if($this->best_answer_id)
+            {
+                return "answerd-accepted";
+            }
+
+            return "answered";
+        }
+
+        return "unanswered";
+    }
 }
