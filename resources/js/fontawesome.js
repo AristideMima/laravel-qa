d77e9b9b9127b87d9397dd9621e34b18b5267f96
aristidemima <aristidemima@gmail.com>
2020-07-18 03:15:15 -0500

    Adding vote-controls with fontawesome using npm

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,9 @@
+import fontawesome from '@fortawesome/fontawesome-free';
+import faCaretUp from '@fortawesome/free-solid-svg-icons/faCaretUp';
+import faCaretDown from '@fortawesome/free-solid-svg-icons/faCaretDown';
+import faStar from '@fortawesome/fontawesome-free-solid/faStar';
+import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
+
+fontawesome.library.add([faCaretUp, faCaretDown, faStar, faCheck]);
+
+dom.watch();
